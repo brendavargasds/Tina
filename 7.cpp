7ee1edb6a4fe1304f7c2c40c93b38291448d7908
brendavargasds <brendakarolinne@gmail.com>
2016-05-24 15:08:51 -0300

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h> 
+
+main()
+
+{
+ 
+int num, resto;
+ 
+printf("informe um numero. \n "); 
+scanf("%d",&num); 
+
+resto = num%2;
+
+if (resto == 0)
+printf("O numero informado eh par.");
+
+else
+printf("O numero informado eh impar.");
+ 
+getch(); 
+
+}
